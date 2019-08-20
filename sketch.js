dff8914d66dfdd480da9063c63152230b8fb74c1
Wesley Dowling <wezzhad@gmail.com>
2019-08-20 14:30:59 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -105,7 +105,7 @@ function draw() {
 //	if(!ready) text('loading model...', width/2, height/2)
 // if(detecting) text('detecting...', width/2, height/2)
   image(capture,0,0, width,height);
-updateDetections();
+//updateDetections();
   //
 	// for (var i = 0; i < blocks.length; i++) {
 	// 	blocks[i].display();
