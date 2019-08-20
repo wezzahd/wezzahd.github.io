129a1b09af6dd56885830568bb57f1f5a58ba0f1
Wesley Dowling <wezzhad@gmail.com>
2019-08-20 13:44:54 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,10 +13,10 @@ async function pre() {
   console.log('loading models')
 	await faceapi.nets.tinyFaceDetector.loadFromUri('models'),
   //await faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models'),
-await faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+await faceapi.nets.faceLandmark68Net.loadFromUri('models'),
 //  await faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
 //  await faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-await faceapi.nets.ageGenderNet.loadFromUri('/models')
+await faceapi.nets.ageGenderNet.loadFromUri('models')
 
 //	await faceapi.loadSsdMobilenetv1Model(url)
   ready = true
