c952319326bee43434c4c8c4a6cb98c274bfce99
Wesley Dowling <wezzhad@gmail.com>
2019-08-29 14:55:17 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ var instruction_toggle = false;
 var gohome = false;
 var reset = false;
 var counter = 0;
-var countermax = 1000;
+var countermax = 200;
 var lerper = 0;
 var inner;
 var reset = false;
