98c839144131c48bd0739a18c3f7f6e1e552ee74
Wesley Dowling <wezzhad@gmail.com>
2020-01-08 12:30:48 +1100

    privacy update

--- sketch_old.js
+++ sketch_old.js
@@ -125,7 +125,7 @@ function draw() {
 
 //console.log(frameRate());
 
-if( updateDetectRunning == false || frameCount <= 100 ){ //test framecount on iphone
+if( updateDetectRunning == false){
   background(0);
   textSize(14);
   fill(255);
@@ -134,18 +134,8 @@ if( updateDetectRunning == false || frameCount <= 100 ){ //test framecount on ip
   image(pixel,width/2,height/2,skip,skip);
   pop();
   textAlign(CENTER,CENTER);
-  textFont("monospace");
+  textFont("Roboto Mono");
   text("loading",width/2,height/2+ 30);
-  textAlign(LEFT,TOP);
-
-  if (width> height){
-    textSize(14);
-  text("Privacy Notice: \nPlease note this website uses webcam image capture and face detection technologies to function.\nWebcam images and detection data is not saved by the website or uploaded to an external server or third party.",width/3,height/2+ 70, width/3, height/3);
-}else{
-  textSize(12);
-  text("Privacy Notice: \nPlease note this website uses webcam image capture and face detection technologies to function.\nWebcam images and detection data is not saved by the website or uploaded to an external server or third party.",width/6,height/2+ 70, width-(width/6*2), height/3);
-
-}
   updateDetections();
   }
 
